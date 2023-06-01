e4ff9eb8857e37312fcd0bd695dbd3e24a17f60c
Robert A Howell <rhowell91@live.com>
2023-06-01 05:05:25 -0700

    Add files via upload

---
+++ SharePoint/SharePoint.NewWebApplication.ps1
@@ -0,0 +1,22 @@
+﻿asnp *SharePoint* -ErrorAction SilentlyContinue
+
+##########################################
+#Create a new web applicatoin
+##Variables
+
+$Name = 'DeveloperPublishing'
+$Port = 5001
+$HostHeader = 'developer.hyperspace.local'
+$ApplicationPool = 'Developer-5001'
+$ApplicationPoolAccount = (Get-SPManagedAccount "HYPERSPACE\spfarm")
+$DatabaseName = 'WSS_Web-DeveloperPublishing'
+
+New-SPWebApplication `
+    -Name $Name `
+    -Port $Port `
+    -HostHeader $HostHeader `
+    -ApplicationPool $ApplicationPool `
+    -ApplicationPoolAccount $ApplicationPoolAccount `
+    -DatabaseName $DatabaseName `
+    -DatabaseServer 'WIN-4T86T2A0TU5' `
+    -AuthenticationMethod 'NTLM' `
---
+++ SharePoint/getWebLists.ps1
@@ -0,0 +1,9 @@
+﻿#list all lists in a sharepoint website
+#visibile and hidden lists namd,ids will be displayed
+$url = "http://sharepoint.hyperspace.local:34881"
+
+function getWebLists($url){
+    $web = get-spweb $url
+    $web.Lists | select title, id, hidden
+}
+getWebLists $url
