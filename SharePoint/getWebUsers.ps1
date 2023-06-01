4286c878ce6336c9ead106029ee3c27dfd48d0be
Robert A Howell <rhowell91@live.com>
2023-06-01 15:56:41 -0700

    Add files via upload

---
+++ SharePoint/getWebUsers.ps1
@@ -0,0 +1,18 @@
+﻿Add-PSSnapin *Microsoft.SharePoint.PowerShell*
+#get the users of sharepoint website:
+$url = "http://developer.hyperspace.local:5001"
+
+#set the output file path
+#$env:USERPROFILE is the local user, so user\documents\<filename>.txt
+$outFilePath = "$($env:USERPROFILE)\documents\webUsers.txt"
+
+function getWebUsers($url) {
+    $web = get-spweb -Identity $url
+    if ($web){
+        Get-SPUser -Web $web |`
+            select name, userlogin, sid, groups | `
+            Out-File -FilePath $outFilePath
+    }
+}
+getWebUsers $url
+ii $outFilePath
