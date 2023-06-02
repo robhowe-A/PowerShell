c0bf01fcc97bbddc074d88fda8f66ccae1bb56c9
Robert A Howell <rhowell91@live.com>
2023-06-01 19:14:37 -0700

    Add files via upload

---
+++ SharePoint/getSPServices.ps1
@@ -0,0 +1,6 @@
+﻿Add-PSSnapin *Microsoft.SharePoint.PowerShell* -ErrorAction SilentlyContinue
+
+Get-SPService | select TypeName, Required, Hidden, NeedsUpgrade |`
+    Sort-Object -Property `
+        @{Expression="Required";Descending="$false"},`
+        @{Expression="TypeName";Descending="$false"}
