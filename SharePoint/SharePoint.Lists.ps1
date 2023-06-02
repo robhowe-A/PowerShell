a4cec710bd21a339f7bb2b62a9ee2fb729b08d09
Robert A Howell <rhowell91@live.com>
2023-06-02 05:44:22 -0700

    Add files via upload

---
+++ SharePoint/SharePoint.Lists.ps1
@@ -0,0 +1,16 @@
+﻿$sites = get-spsite
+$sites.AllWebs
+
+$web = get-spweb http://sharepoint.hyperspace.local:34881  
+#$web.Lists | select * -ExcludeProperty *xml* -First 1
+$web.Lists | select title, id, hidden
+
+#list all lists in a sharepoint website
+#visibile and hidden lists namd,ids will be displayed
+$url = "http://sharepoint.hyperspace.local:34881"
+
+function getWebLists($url){
+    $web = get-spweb $url
+    $web.Lists | select title, id, hidden
+}
+getWebLists $url
