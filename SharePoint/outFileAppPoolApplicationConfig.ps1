4ee824b99f856d0369d79e24f439c2cc2bb7aa0e
Robert A Howell <rhowell91@live.com>
2023-06-03 16:17:06 -0700

    Add files via upload

---
+++ SharePoint/outFileAppPoolApplicationConfig.ps1
@@ -0,0 +1,46 @@
+﻿<#
+#Description: This script outputs configuration details of the applications running in a selected app pool
+#Author: Robert Howell
+#Parameters: 
+    -$AppPoolNames - Taking in a commma separated array of app pool names
+    -$FilePath - The output file path
+
+    #EX: 
+        PS PowerShell> $AppPoolNames = @('DefaultAppPool', 'Developer-5001')
+        PS PowerShell> .\outFileAppPoolApplicationConfig.ps1 -AppPoolNames $AppPoolNames -FilePath "C:\Users\spfarm\Documents\SharePoint_Data\ApplicationsConfig.txt"
+
+#>
+param(
+    [Parameter(
+        Mandatory=$false,
+        HelpMessage="App pool name(s) separated by a ','. 
+        Default: 
+            #DefaultAppPool, 
+            #SecurityTokenServiceApplicationPool, 
+            #SharePoint Web Services System"
+        )]
+    [string[]]$AppPoolNames = @('DefaultAppPool', 'SecurityTokenServiceApplicationPool', 'SharePoint Web Services System'),
+    [Parameter(
+        Mandatory=$false,
+        HelpMessage="Object output file location."
+        )]
+    [string]$FilePath = "$($env:USERPROFILE)\documents\appPoolApplicationData.txt"
+)
+
+Start-Transcript $FilePath
+#loop through application pool name(s) of ingress parameter data
+foreach($appPool in $AppPoolNames){
+    #Friendly data output separates each config data set, per application pool
+    Write-Host "#####################################"
+    Write-Host "Application Pool: " -ForegroundColor DarkCyan -NoNewline
+    Write-Host "$($appPool)" -ForegroundColor Cyan
+    Write-Host "#####################################`n"
+
+    #Get IIS config data for the application
+    #Output set is a 'Microsoft.Web.Administration.ConfigurationElement' object type
+    Get-IISSite | `
+        Where-Object{$_.applications.ApplicationPoolName -match $appPool} | `
+        select -ExpandProperty applications
+}
+Stop-Transcript
+ii $FilePath
